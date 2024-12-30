bb0914cf655b29f74672318dc172bdc920a5ad8d
vikinglock <119771411+Vikinglock@users.noreply.github.com>
2024-12-30 02:09:22 -0500

    dear imgui WIP
    surprisingly dear imgui didn't take that long but i gotta get this out immediately cause i got a thing i gotta do with it

--- run.sh
+++ run.sh
@@ -10,10 +10,10 @@ OUTPUT=$2
 shift 2
 
 if [ "$MODE" = "-metal" ]; then
-    clang++ "$@" -x objective-c++ bento/metal/metal.mm bento/metal/metaltexture.mm bento/lib/hidapi/mac/hid.c -Wno-null-character -std=c++17 -framework Metal -framework QuartzCore -framework Cocoa -framework IOKit -framework MetalKit -framework CoreVideo -framework Foundation -framework CoreFoundation -framework Carbon -o "$OUTPUT" -DUSE_METAL || exit 1 
+    clang++ "$@" -x objective-c++ bento/metal/metal.mm bento/metal/metaltexture.mm bento/lib/hidapi/mac/hid.c bento/lib/imgui/imgui.cpp bento/lib/imgui/imgui_demo.cpp bento/lib/imgui/imgui_draw.cpp bento/lib/imgui/imgui_tables.cpp bento/lib/imgui/imgui_widgets.cpp bento/lib/imgui/backends/imgui_impl_metal.mm -Wno-null-character -std=c++17 -framework Metal -framework QuartzCore -framework Cocoa -framework IOKit -framework MetalKit -framework CoreVideo -framework Foundation -framework CoreFoundation -framework Carbon -o "$OUTPUT" -DUSE_METAL || exit 1 
 elif [ "$MODE" = "-opengl" ]; then
     clang -c bento/glad.c -o glad.o || exit 1
-    clang++ "$@" bento/opengl/opengl.cpp bento/opengl/opengltexture.cpp glad.o -std=c++17 -framework OpenGL -framework QuartzCore -framework Cocoa -framework IOKit -framework CoreVideo -framework CoreFoundation -framework Carbon -lglfw3 -o "$OUTPUT" -DUSE_OPENGL || exit 1
+    clang++ "$@" bento/opengl/opengl.cpp bento/opengl/opengltexture.cpp glad.o bento/lib/imgui/imgui.cpp bento/lib/imgui/imgui_demo.cpp bento/lib/imgui/imgui_draw.cpp bento/lib/imgui/imgui_tables.cpp bento/lib/imgui/imgui_widgets.cpp bento/lib/imgui/backends/imgui_impl_opengl3.cpp bento/lib/imgui/backends/imgui_impl_glfw.cpp -std=c++17 -framework OpenGL -framework QuartzCore -framework Cocoa -framework IOKit -framework CoreVideo -framework CoreFoundation -framework Carbon -lglfw3 -o "$OUTPUT" -DUSE_OPENGL || exit 1
     rm glad.o
 else
     echo "Usage: $0 (-metal|-opengl) <output_name> <c++ file> <another c++ file> ..."
