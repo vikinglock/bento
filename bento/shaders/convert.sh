8c80525f06e222667bc639c1e16e86e2228a5ece
vikinglock <119771411+Vikinglock@users.noreply.github.com>
2025-01-08 04:51:28 -0500

    multiple lights
    it now supports multiple lights and is ready for release
    also openal tested on linux and windows

--- bento/shaders/convert.sh
+++ bento/shaders/convert.sh
@@ -14,43 +14,57 @@ usage() {
 if [ "$#" -lt 4 ]; then
     usage
 fi
-glslangValidator -V --quiet $1 -o "vert.spv" || exit 1
-glslangValidator -V --quiet $2 -o "frag.spv" || exit 1
 
 case "$3" in
     --metal)
         if [ "$#" -ne 5 ]; then usage; fi
+        glslangValidator -V --quiet $1 -o "vert.spv" || exit 1
+        glslangValidator -V --quiet $2 -o "frag.spv" || exit 1
         spirv-cross "vert.spv" --msl --output $4 || exit 1
         spirv-cross "frag.spv" --msl --output $5 || exit 1
-        ./bento/shaders/bindfix $1 $4 || clang++ bento/shaders/bindingsfix.cpp -o bento/shaders/bindfix ; ./bento/shaders/bindfix $1 $4
+        ./bento/shaders/bindfix $1 $4 || (clang++ bento/shaders/bindingsfix.cpp -o bento/shaders/bindfix ; ./bento/shaders/bindfix $1 $4)
+        rm "vert.spv" "frag.spv"
         ;;
     --opengl330)
         if [ "$#" -ne 5 ]; then usage; fi
+        glslangValidator -V --quiet $1 -o "vert.spv" || exit 1
+        glslangValidator -V --quiet $2 -o "frag.spv" || exit 1
         spirv-cross "vert.spv" --version 330 --output $4 || exit 1
         spirv-cross "frag.spv" --version 330 --output $5 || exit 1
+        rm "vert.spv" "frag.spv"
         ;;
     --opengl330c)
         if [ "$#" -ne 5 ]; then usage; fi
+        glslangValidator -V --quiet $1 -o "vert.spv" || exit 1
+        glslangValidator -V --quiet $2 -o "frag.spv" || exit 1
         spirv-cross "vert.spv" --version 330 --output $4 || exit 1
         spirv-cross "frag.spv" --version 330 --output $5 || exit 1
-        ./bento/shaders/330c $4 $5 || clang++ bento/shaders/330to330core.cpp -std=c++11 -o bento/shaders/330c || echo "could not find a required library script file thing (for conversion), try running it in a folder where bento/ is present"
+        ./bento/shaders/330c $4 $5 || (clang++ bento/shaders/330to330core.cpp -std=c++11 -o bento/shaders/330c ; ./bento/shaders/330c $4 $5)
         ;;
     --vulkan)
         if [ "$#" -ne 4 ]; then usage; fi
+        glslangValidator -V --quiet $1 -o "vert.spv" || exit 1
+        glslangValidator -V --quiet $2 -o "frag.spv" || exit 1
         cat "vert.spv" "frag.spv" > $4
+        rm "vert.spv" "frag.spv"
         ;;
     --hlsl)
         if [ "$#" -ne 5 ]; then usage; fi
+        glslangValidator -V --quiet $1 -o "vert.spv" || exit 1
+        glslangValidator -V --quiet $2 -o "frag.spv" || exit 1
         spirv-cross "vert.spv" --hlsl --output $4 || exit 1
         spirv-cross "frag.spv" --hlsl --output $5 || exit 1
+        rm "vert.spv" "frag.spv"
         ;;
     --spirv)
         if [ "$#" -ne 5 ]; then usage; fi
+        glslangValidator -V --quiet $1 -o "vert.spv" || exit 1
+        glslangValidator -V --quiet $2 -o "frag.spv" || exit 1
         cp "vert.spv" $4
         cp "frag.spv" $5
+        rm "vert.spv" "frag.spv"
         ;;
     *)
         usage
         ;;
 esac
-rm "vert.spv" "frag.spv"
--- run.bat
+++ run.bat
@@ -3,8 +3,11 @@ if "*%~1*" == "" (
     echo Usage: run.bat ^<output_name^> ^<c++ file^> [another c++ file ...]
     exit /b 1
 )
+
+set output=%1
+
 clang -c bento/glad.c -o glad.o
-clang++ *%** glad.o bento/opengl/opengl.cpp -I./bento/lib -L./bento/lib/GLFW/x64 ^
+clang++ %2 glad.o bento/opengl/opengl.cpp bento/opengl/opengltexture.cpp -I./bento/lib -L./bento/lib/GLFW/x64 ^
     bento/lib/imgui/imgui.cpp ^
     bento/lib/imgui/imgui_demo.cpp ^
     bento/lib/imgui/imgui_draw.cpp ^
@@ -12,15 +15,16 @@ clang++ *%** glad.o bento/opengl/opengl.cpp -I./bento/lib -L./bento/lib/GLFW/x64
     bento/lib/imgui/imgui_widgets.cpp ^
     bento/lib/imgui/backends/imgui_impl_opengl3.cpp ^
     bento/lib/imgui/backends/imgui_impl_glfw.cpp ^
-    -I./bento/lib/include/AL ^
     -L./bento/lib/build/Release ^
+    -I./bento/lib/AL -L./bento/lib/openal-soft/libs/Win64 -lOpenAL32 ^
     -lglfw3 -lopengl32 -luser32 -lgdi32 -lole32 -loleaut32 ^
     -luuid -lshell32 -lkernel32 -lmsvcrt ^
-    -o bento.exe -DUSE_OPENGL -Wno-deprecated -D_CRT_SECURE_NO_WARNINGS
-
+    -o "%output%" -DUSE_OPENGL -DWINDOWS -Wno-deprecated -D_CRT_SECURE_NO_WARNINGS
 if %errorlevel% equ 0 (
     echo Build succeeded.
     %1
 ) else (
     echo Build failed.
 )
+
+del glad.o
